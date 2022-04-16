369726a8cef5ae071bafb74291a4fdb1e1a37ec6
Thiago <thiduzz14@gmail.com>
2022-04-16 17:05:15 +0200

    Adjust main page responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,12 +34,12 @@ const Home: NextPage = () => {
                             <h1 className="text-5xl flex flex-row items-center">Hi, I&apos;m <div
                                 className="rounded-xl mx-2 p-2 bg-gray-800 text-white">Thiago</div></h1>
                         </div>
-                        <div className="subtitle uppercase rounded-lg mx-2 mb-5 py-2 text-gray-800 flex gap-x-6 px-10 tracking-wide">
+                        <div className="subtitle uppercase rounded-lg mx-2 mb-5 py-2 text-gray-800 flex gap-x-6 gap-y-4 md:gap-y-0 px-10 tracking-wide justify-center md:justify-between flex-wrap text-center">
                             <span>full-stack developer</span>
                             <span>solutions engineer</span>
                             <span>blockchain enthusiast</span>
                         </div>
-                        <div className="flex flex-row justify-center items-center gap-10">
+                        <div className="flex flex-row justify-center items-center gap-10 flex-wrap mb-10 md:mb-0">
                             <Link href={githubLink} passHref>
                                 <div className="bg-gray-800 rounded-full p-6 flex flex-wrap cursor-pointer hover:scale-125 duration-300">
                                     <FiGithub className="text-white text-2xl"/>
