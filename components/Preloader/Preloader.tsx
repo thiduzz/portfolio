97b6978605996ddb75de81a7c8c833741f846086
Thiago <thiduzz14@gmail.com>
2022-04-16 13:47:11 +0200

    Test preloader

---
+++ components/Preloader/Preloader.tsx
@@ -0,0 +1,6 @@
+
+import React from 'react'
+
+const Preloader = () => (<div className="h-screen w-screen bg-red-700 flex items-center justify-center">Loading...</div>)
+
+export default Preloader
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+import Preloader from "../components/Preloader/Preloader";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   const handleStop = () => setLoading(true)
+  //   const handleStart = () => setLoading(true)
+  //   router.events.on('routeChangeStart', handleStart)
+  //   router.events.on('routeChangeComplete', handleStop)
+  //   router.events.on('routeChangeError', handleStop)
+  //   return () => {
+  //     router.events.off('routeChangeStart', handleStart)
+  //     router.events.off('routeChangeComplete', handleStop)
+  //     router.events.off('routeChangeError', handleStop)
+  //   }
+  // },[])
+  // const path = (/#!(\/.*)$/.exec(router.asPath) || [])[1];
+  // console.log(path)
+  // if (path) {
+  //   router.replace(path);
+  // }
+  return loading ? <Preloader/> : <Component {...pageProps} />
 }
 
 export default MyApp
