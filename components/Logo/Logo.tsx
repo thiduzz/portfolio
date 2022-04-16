b9a028113e8c38487aef58ba7bb4a0bf248e4906
Thiago <thiduzz14@gmail.com>
2022-04-16 14:44:06 +0200

    Adjust logo to be in a package-name format ;P

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -5,7 +5,7 @@ import {FiChevronRight, FiAtSign} from 'react-icons/fi'
 const Logo = () => {
   return <Link href="/" passHref>
     <div className='text-lg cursor-pointer text-bold flex items-center'>
-      <span className='animate-blink text-green-500 flex flex-row items-center'><FiChevronRight></FiChevronRight>_</span>thiago mello <span className='text-green-500'><FiAtSign/></span><Link href="https://github.com/thiduzz/portfolio/releases"><span className='text-green-500'>1.0.0</span></Link>
+      <span className='animate-blink text-green-500 flex flex-row items-center'><FiChevronRight></FiChevronRight>_</span>thiago-mello <span className='text-green-500'><FiAtSign/></span><Link href="https://github.com/thiduzz/portfolio/releases"><span className='text-green-500'>1.0.0</span></Link>
     </div>
   </Link>
 }
