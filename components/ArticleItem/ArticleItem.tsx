15726f38dd791f8301b861b799c81ec4373d0ce1
Thiago <thiduzz14@gmail.com>
2022-04-17 11:47:13 +0200

    Review listing of articles - introduce new types, introduce new fields to query

--- components/ArticleItem/ArticleItem.tsx
+++ components/ArticleItem/ArticleItem.tsx
@@ -2,20 +2,36 @@ import React from 'react'
 import { ArticleItemProp } from './ArticleItem.types'
 import Link from "next/link";
 import Image from "next/image";
+import {dayjs} from "../../lib/day";
+
 
 
 const ArticleItem = ({ article }: ArticleItemProp) => {
-    return <Link href={`/articles/${encodeURIComponent(article.slug)}`} passHref>
-        <div className="bg-white rounded-md shadow-md w-full">
-            <div className="relative shadow-2xl h-48 w-full rounded-t-md">
-                <Image src="/placeholder.jpeg" className="text-center m-0 p-0 w-full  rounded-t-md h-full object-cover object-center" layout="fill"/>
-            </div>
-            <div className="p-5">
-                <h3 className="text-xl line-clamp-3">{article.title}</h3>
-                <p className="line-clamp-2">{article.text}</p>
+    const imageUrl = article.image ? article.image.url : "/placeholder.jpeg"
+    const linkUrl = `/articles/${encodeURIComponent(article.slug)}`
+    // @ts-ignore
+    const publishDateString = article.publishedAt ? dayjs(article.publishedAt).fromNow() : null
+    return <div className="article-wrapper">
+            <div className="flex flex-col justify-start items-end bg-white rounded-md shadow-md w-full mb-5 md:mb-0">
+                <Link href={linkUrl} passHref>
+                <div className="relative shadow-2xl h-48 w-full rounded-t-md cursor-pointer">
+                    <Image src={imageUrl} className="text-center m-0 p-0 w-full  rounded-t-md h-full object-cover object-center" layout="fill"/>
+                </div>
+                </Link>
+                <div className="p-5 flex flex-col justify-around max-h-max">
+                    <div className="grow">
+                        <h3 className="text-lg line-clamp-3">{article.title}</h3>
+                        <p className="line-clamp-4 mt-5">{article.excerpt}</p>
+                    </div>
+                    <div className="w-full text-sm text-green-500 mt-3">
+                        <div className="flex flex-row justify-between w-full">
+                            <Link href={linkUrl} passHref><button className="hover:underline hover:opacity-70 cursor-pointer">Read more...</button></Link>
+                            {publishDateString && <span>{publishDateString}</span>}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </Link>
 }
 
 export default ArticleItem
--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,64 +1,86 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import Layout from "../../components/Layout";
 import Head from "next/head";
 import React, {useCallback, useEffect, useState} from "react";
-import { gql } from "@apollo/client";
+import {gql} from "@apollo/client";
 import sanity from "../../lib/sanity";
-import {IArticle} from "../../types/article";
-import Link from "next/link";
+import {IArticle, IArticleImage, IArticleListResponse} from "../../types/article";
 import ArticleItem from "../../components/ArticleItem";
 
 const Articles: NextPage = () => {
-  const [articles, setArticles] = useState<Array<IArticle>>([])
+    const [articles, setArticles] = useState<Array<IArticle>>([])
 
-  const loadArticles = useCallback(async () => {
-      const response = await sanity.query({
-          query: gql`
+    const loadArticles = useCallback(async () => {
+        const response = await sanity.query({
+            query: gql`
             query{
                 allPost{
                     _id,
                     title,
+                    excerpt,
                     slug {
                         current
-                    }
+                    },
+                    mainImage{
+                        asset{
+                            url,
+                            title,
+                            description,
+                            altText
+                        }
+                    },
+                    publishedAt
                 }
             }
       `,
-      });
-      if(response){
-          setArticles(response.data.allPost.map(
-              (item: { _id: string; title: string; slug: { current: string } }) => {
-              return {id: item._id, title: item.title, slug: item.slug.current, }
-             }
-          ) as Array<IArticle>)
-      }
-  },[])
+        });
+        if (response) {
+            setArticles(response.data.allPost.map((item: IArticleListResponse) => {
+                    let image: IArticleImage|null = null;
+                    if(item.mainImage !== null){
+                        image = {
+                            url: item.mainImage.asset.url,
+                            title: item.mainImage.asset.title,
+                            description: item.mainImage.asset.description,
+                            alt: item.mainImage.asset.title,
+                        }
+                    }
+                    return {
+                        id: item._id,
+                        title: item.title,
+                        slug: item.slug.current,
+                        excerpt: item.excerpt,
+                        image,
+                        publishedAt: item.publishedAt ? new Date(item.publishedAt) : null
+                    } as IArticle
+                }
+            ) as Array<IArticle>)
+        }
+    }, [])
 
+    useEffect(() => {
+        loadArticles()
+    }, [])
 
-
-  useEffect(() => {
-      loadArticles()
-  },[])
-
-  return (
-      <Layout>
-        <Head>
-          <title>Thiago Mello - Articles</title>
-          <meta name="description" content="Collection of articles and others" />
-          <link rel="icon" href="/public/favicon.ico" />
-        </Head>
-        <div className="page-content">
-            <div className="my-10">
-                <div className="flex justify-center md:justify-start w-full">
-                    <h1 className="text-3xl text-left">Articles</h1>
-                </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center mt-10">
-                    {articles && articles.map((article) => <ArticleItem key={article.id} article={article}/>)}
+    return (
+        <Layout>
+            <Head>
+                <title>Thiago Mello - Articles</title>
+                <meta name="description" content="Collection of articles and others"/>
+                <link rel="icon" href="/public/favicon.ico"/>
+            </Head>
+            <div className="page-content">
+                <div className="my-10">
+                    <div className="flex justify-center md:justify-start w-full">
+                        <h1 className="text-3xl text-left">Articles</h1>
+                    </div>
+                    <div className="md:grid md:grid-cols-2 md:gap-4 justify-center mt-10">
+                        {articles && articles.map((article) => <ArticleItem key={article.id} article={article}/>)}
+                    </div>
                 </div>
             </div>
-        </div>
-      </Layout>
-  )
+        </Layout>
+    )
 }
 
 export default Articles
