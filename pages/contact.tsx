ec169128949d6f803747ac95f3bde9637cc6544e
Thiago <thiduzz14@gmail.com>
2022-04-17 18:28:29 +0200

    Add placeholder for contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Layout from "@components/Layout";
+import Head from "next/head";
+import React from "react";
+
+const About: NextPage = () => {
+  return (
+      <Layout>
+
+        <Head>
+          <title>Thiago Mello - Contact</title>
+          <meta name="description" content="A little about me..." />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="page-content">
+            <div className="my-10 flex justify-center md:justify-start w-full">
+                <h1 className="text-3xl text-left">Contact me...</h1>
+            </div>
+        </div>
+      </Layout>
+  )
+}
+
+export default About
