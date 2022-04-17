92eb4ce1bd4fb692653088d6bb4d74dac5669900
Thiago <thiduzz14@gmail.com>
2022-04-17 11:46:05 +0200

    Add Article Entity types

--- types/article.d.ts
+++ types/article.d.ts
@@ -1,7 +1,40 @@
-
 export interface IArticle {
     id: string
     title: string
-    text: any
+    excerpt: string
     slug: string
+    image?: IArticleImage
+    publishedAt: Date
+    content?: IArticleContent
+    tags?: Array<string>
+    categories?: Array<string>
+}
+
+export interface IArticleImage {
+    title: string
+    description: string
+    alt: string
+    url: string
+}
+
+export interface IArticleContent {
+    isMarkdown: boolean
+    markdown: string
+    richtext: any
+}
+
+export interface IArticleListResponse {
+    _id: string;
+    title: string;
+    excerpt: string;
+    slug: { current: string };
+    mainImage: {
+        asset: {
+            url: string;
+            title: string;
+            description: string;
+            altText: string;
+        }
+    }|null
+    publishedAt: Date|null;
 }
