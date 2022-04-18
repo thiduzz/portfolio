4003884ddee92c19f41078a44886e3a9685fa42d
Thiago <thiduzz14@gmail.com>
2022-04-18 10:55:24 +0200

    Add UTC plugin for RSS feed date format

--- libs/day.ts
+++ libs/day.ts
@@ -1,6 +1,10 @@
 import * as _dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
+import utc from 'dayjs/plugin/utc';
+
+
 
 export const dayjsRelative = _dayjs.extend(relativeTime);
 export const dayjsFormatted = _dayjs.extend(localizedFormat);
+export const dayjsUTC = _dayjs.extend(utc);
