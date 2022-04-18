fb61f1bd1c9fd50a139d91f4142ddefe01a73a03
Thiago <thiduzz14@gmail.com>
2022-04-18 12:09:58 +0200

    Add dots pattern

--- components/ArticleHeader/ArticleHeader.tsx
+++ components/ArticleHeader/ArticleHeader.tsx
@@ -5,8 +5,9 @@ import {ArticleHeaderProp} from "@components/ArticleHeader/ArticleHeader.types";
 const ArticleHeader = ({ label, title, description  }: ArticleHeaderProp) => {
     return <div className="flex justify-center md:justify-start w-full flex-col">
         <div className="flex justify-center md:justify-start w-full flex-row items-center">
-            {label && <span className="text-xl">{label}:</span>}
+            {label && <span className="text-xl">{label}:&nbsp;</span>}
             <h1 className={`text-3xl text-left ${label ? 'md:ml-5' : ''}`}>{title}</h1>
+            <div className="dots" aria-hidden="true"></div>
         </div>
         {description && <h3 className="text-xl mt-4">{description}</h3>}
     </div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,8 @@ const About: NextPage = () => {
         <StandardHead title="Thiago Mello - About" description="Personal things and a bit of my story so far - including career, upbringing and a bit of my personality"/>
         <div className="page-content">
             <div className="my-10 flex justify-center md:justify-start w-full">
-                <h1 className="text-3xl text-left">About me...</h1>
+                <div className="dots" aria-hidden="true"></div>
+                <h1 className="text-3xl text-left">About</h1>
             </div>
         </div>
       </Layout>
