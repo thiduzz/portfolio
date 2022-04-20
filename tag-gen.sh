4b15493512c826936c610eb5d614e981ca1896e6
Thiago <thiduzz14@gmail.com>
2022-04-20 03:51:15 +0200

    Add cat

--- tag-gen.sh
+++ tag-gen.sh
@@ -2,4 +2,11 @@
 
 TAG=$(cat package.json | grep -Po '"version":.*?[^\\]",' | awk -F':' '{print $2}' | sed -e 's/^[[:space:]]*//' | sed 's/^\|,\|"//g')
 
-echo "\nNEXT_PUBLIC_RELEASE_TAG=${TAG}" >> ".env.production"
+echo "\nNEXT_PUBLIC_RELEASE_TAG=${TAG}" >> "/vercel/path0/.env.production"
+
+echo "Value updated!"
+cat /vercel/path0/.env.production
+
+echo "Waiting..."
+sleep 2
+echo "Awake..."
