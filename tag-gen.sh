8b5f3d51473c03381941a761dcd0af1f76594754
Thiago <thiduzz14@gmail.com>
2022-04-20 03:45:41 +0200

    Replace with an awk/sed approach

--- tag-gen.sh
+++ tag-gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-TAG=$(cat package.json | jq -r '.version' )
+TAG=$(cat package.json | grep -Po '"version":.*?[^\\]",' | awk -F':' '{print $2}' | sed -e 's/^[[:space:]]*//' | sed 's/^\|,\|"//g')
 
 echo "\nNEXT_PUBLIC_RELEASE_TAG=${TAG}" >> ".env.production"
