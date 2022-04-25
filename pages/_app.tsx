3267124a86f1a7432bd48b595f416e9d3f750c10
Thiago <thiduzz14@gmail.com>
2022-04-26 01:31:38 +0200

    Add GTM package andtrigger it on app load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useEffect} from "react";
+import TagManager from "react-gtm-module"
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const {klaro} = useConsent()
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
+    })
+  }, [klaro])
   return <Component {...pageProps} />
 }
 
