e4a7f8ebfa2661cc2c8066444fd6ddc6ba573181
Thiago <thiduzz14@gmail.com>
2022-04-27 00:41:58 +0200

    Remove AdSense consent until maturing a monetization strategy

--- libs/klaro-consentmanager.ts
+++ libs/klaro-consentmanager.ts
@@ -31,17 +31,17 @@ const KlaroConfig = {
             consentModal: {
                 title: 'Privacy Settings',
                 description:
-                    'This website store and access cookies on your device - these cookies helps the maintainer understand on how to improve the user experience of the website and its service, displaying personalized recommendations and advertising. <br/><br/>You can change or revoke this at any time in the privacy settings.',
+                    'This website store and access cookies on your device - these cookies helps the maintainer understand on how to improve the user experience of the website and its service. <br/><br/>You can change or revoke this at any time in the privacy settings.',
             },
 
             googleAnalytics: {
                 description: 'Example of an inline tracking script',
                 title: 'Google Analytics Tracking',
             },
-            adsense: {
-                description: 'Displaying of advertisements',
-                title: 'Google Adsense Advertisement',
-            },
+            // adsense: {
+            //     description: 'Displaying of advertisements',
+            //     title: 'Google Adsense Advertisement',
+            // },
             purposes: {
                 analytics: 'Analytics',
                 security: 'Security',
@@ -66,11 +66,11 @@ const KlaroConfig = {
             optOut: false,
             onlyOnce: true,
         },
-        {
-            name: 'google-adsense',
-            title: 'Google AdSense',
-            purposes: ['advertising'],
-        },
+        // {
+        //     name: 'google-adsense',
+        //     title: 'Google AdSense',
+        //     purposes: ['advertising'],
+        // },
     ],
 };
 
