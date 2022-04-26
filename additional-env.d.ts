57f1a52ee3f530d7e445e690bb308c69398aa2eb
Thiago <thiduzz14@gmail.com>
2022-04-27 00:40:25 +0200

    Register Typescript to SyntaxHighlighter

--- additional-env.d.ts
+++ additional-env.d.ts
@@ -1,5 +1,11 @@
+import {Light as SyntaxHighlighter} from "react-syntax-highlighter";
+import typescript from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
+
 interface Window {
     location: any;
     klaro: any;
     klaroConfig: any;
 }
+
+SyntaxHighlighter.registerLanguage('typescript',typescript)
+SyntaxHighlighter.registerLanguage('tsx',typescript)
