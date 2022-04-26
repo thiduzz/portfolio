2e0cad23138cc8edf7af48fe9d756f5c853b5402
Thiago <thiduzz14@gmail.com>
2022-04-27 00:40:48 +0200

    Review about me format

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,93 +15,150 @@ const About: NextPage = () => {
             <StandardHead title="Thiago Mello - About" image={mainImage}
                           description="Personal things and a bit of my story so far - including career, upbringing and a bit of my personality"/>
             <div className="page-content">
-                <div className="my-10 flex justify-center md:justify-start w-full">
-                    <div className="dots" aria-hidden="true"></div>
-                    <h1 className="text-3xl text-left">A little about me...</h1>
-                </div>
-                <div className="flex  flex-col md:flex-row">
-                    <div className="w-full lg:w-1/3 flex items-center justify-start px-5 md:pt-24 lg:pt-5 flex-col">
-                        <Image
-                            src={mainImage}
-                            className="rounded-md text-center m-0 p-0 w-full h-full object-cover object-center shadow-2xl"
-                            alt="Thiago's Photo taken in Kyoto - Japan Picture" title="Thiago in Kyoto - Japan"
-                            height="400" width="500"
-                        />
-                        <figcaption className="text-xs mt-2">Trip to Kyoto, Japan - 2019</figcaption>
+                <div className="container max-w-4xl">
+                    <div className="my-10 flex justify-center md:justify-start w-full">
+                        <div className="dots" aria-hidden="true"></div>
+                        <h1 className="text-3xl text-left">A little about me...</h1>
                     </div>
-                    <div className='w-full lg:w-2/3 px-5'>
-                        <h2 className="lg:hidden my-5 text-2xl text-center underline underline-offset-8 decoration-double decoration-green-500">Personal</h2>
-                        <p className="pt-5">
-                            I am a Portuguese/Brazilian Software Engineer based in Berlin, Germany - and I currently
-                            work at <Link href="https://www.smava.de" passHref><a target="_blank">smava</a></Link>. I was born and raised in the city
-                            of <Link href="https://en.wikipedia.org/wiki/Curitiba">Curitiba, Brazil</Link> where I also
-                            attended the <Link href="https://www.pucpr.br/cursos-graduacao/sistemas-de-informacao/" passHref><a target="_blank">Information
-                            Systems Bachelor degree at PUC-PR</a></Link> and my post-graduation course in Embedded Systems
-                            and Internet of Things at <Link href="https://www.up.edu.br/" passHref><a target="_blank">Positivo University
-                            (UP)</a></Link>.
-                        </p>
-                        <p className="pt-5">
-                            When I am not coding or studying a technology/programming language - you will find me
-                            scavenging for Vinyls in Berlin&apos;s used Records stores, having an amazing third-wave coffee
-                            in a place such as <Link href="https://bonanzacoffee.de/" passHref><a target="_blank">Bonanza Roastery</a></Link> or running
-                            around with one of my cameras and taking photos of landscapes/abandoned places.
-                        </p>
-                        <p className="pt-5">
-                            I already travelled to 37 countries and lived long-term in 3 of them — Brazil, Czech
-                            Republic and Germany — and had some short periods of my life in Greece and the US. I think
-                            that by expanding my own horizon and by discovering new realities, I can continuously be
-                            able to rediscover and reinvent myself. And yes, Japan and Iceland are my all time favorite
-                            countries!
-                        </p>
-                        <p className="pt-5">
-                            My family is my strength and motivation - I couldn&apos;t be half of the man I am today and have
-                            reached what I reached in my career and life if it wasn&apos;t for my amazing parents,
-                            grand-parents and my sisters. I dearly miss them (in portuguese - saudades) and constantly
-                            look forward to see them - which I am lucky to be able to do at least twice a year :)
-                        </p>
-                    </div>
-                </div>
+                    <h2 className="lg:hidden my-5 text-2xl text-center underline underline-offset-8 decoration-double decoration-green-500">Personal</h2>
 
-
-                <div className="flex flex-col md:flex-row mt-10">
-                    <div className="w-full lg:w-2/3 px-5">
-                        <h2 className="lg:hidden my-5 text-2xl text-center underline underline-offset-8 decoration-double decoration-green-500">Career
-                            & Technology</h2>
-                        <p className="pt-5">
-                            I consider Golang and Javascript (ReactJS/Vue.js) as my main programming languages — I love
-                            it! Prior to that I worked with PHP (Laravel), NodeJS and some other corporate languages
-                            (SAS Base, ACL...). I also really enjoy doing DevOps, Cloud Engineering with Terraform, AWS
-                            and Kubernetes. I see myself as a Solutions Engineer (<i>&quot;give me a problem and I will find
-                            a good solution&quot;</i>) and trail my career as a Full-stack developer - a position which
-                            enables me to traverse and learn the entire stack and have a full understanding of the
-                            solution with a greater impact.
-                        </p>
-                        <p className="pt-5">
-                            For the past months I&apos;ve taken a really big interest in Blockchain, Decentralized networks
-                            and dApps development, studying Rust and Solidity - the programming languages that power the
-                            main blockchains - I am fascinated with the possibilities of Blockchain and I believe that
-                            underneath the &quot;noise&quot; of cryptocurrencies it lies a technology that can definitely disrupt
-                            society in the same level the Internet did in the 90&apos;s/2000&apos;s.
-                        </p>
-                        <p className="pt-5">
-                            I tend to see and approach programming languages/technology as a means to an end — don&apos;t get
-                            me wrong, they are really important and need to be taken seriously — but let&apos;s not confuse
-                            the real purpose of it, which in my case, I believe it is to help, enable and unite people.
-                        </p>
-                        <p className="py-5">
-                            You can find more information about of my career on <Link href={linkedinLink}
-                                                                                      passHref><a target="_blank">LinkedIn</a></Link> or my CV
-                            (<Link href="/docs/Thiago_Mello_CV.pdf" passHref><a target="_blank">download here</a></Link>)
-                            and find some of my projects in <Link href={githubLink} passHref><a target="_blank">Github</a></Link>.
-                        </p>
+                    <div className="flex  flex-col md:flex-row">
+                        <div className="w-full lg:w-1/3 flex items-center justify-start px-5 md:pt-24 lg:pt-5 flex-col">
+                            <Image
+                                src={mainImage}
+                                className="rounded-md text-center m-0 p-0 w-full h-full object-cover object-center shadow-2xl"
+                                alt="Thiago's Photo taken in Kyoto - Japan Picture" title="Thiago in Kyoto - Japan"
+                                height="400" width="500"
+                            />
+                            <figcaption className="text-xs mt-2">Trip to Kyoto, Japan - 2019</figcaption>
+                        </div>
+                        <div className='w-full lg:w-2/3 px-5'><p className="pt-5">
+                                I am a Portuguese/Brazilian Software Engineer based in Berlin, Germany - and I currently
+                                work at <Link href="https://www.smava.de" passHref><a target="_blank">smava</a></Link>.
+                                I was born and raised in the city
+                                of <Link href="https://en.wikipedia.org/wiki/Curitiba">Curitiba, Brazil</Link> where I
+                                also
+                                attended the <Link href="https://www.pucpr.br/cursos-graduacao/sistemas-de-informacao/"
+                                                   passHref><a target="_blank">Information
+                                Systems Bachelor degree at PUC-PR</a></Link> and my post-graduation course in Embedded
+                                Systems
+                                and Internet of Things at <Link href="https://www.up.edu.br/" passHref><a
+                                target="_blank">Positivo University
+                                (UP)</a></Link>.
+                            </p>
+                            <p className="pt-5">
+                                When I am not coding or studying a technology/programming language - you will find me
+                                scavenging for Vinyls in Berlin&apos;s used Records stores, having an amazing third-wave
+                                coffee
+                                in a place such as <Link href="https://bonanzacoffee.de/" passHref><a target="_blank">Bonanza
+                                Roastery</a></Link> or running
+                                around with one of my cameras and taking photos of landscapes/abandoned places.
+                            </p>
+                        </div>
                     </div>
-                    <div className='w-full lg:w-1/3 flex items-end justify-center px-5'>
-                        <Image
-                            src="https://cdn.sanity.io/images/qgimqc2y/portfolio/f7b9f3e9404ce427b1a763d5d0ea0f3001e8f0bf-1300x1392.png?w=2000&fit=max&auto=format"
-                            className="text-center m-0 p-0 w-full h-full object-cover object-center shadow-2xl"
-                            alt="Gophercized Thiago's" title="Thiago's Gopher"
-                            height="300" width="300"
-                        />
+                    <div className="flex flex-col-reverse md:flex-row mt-10">
+                        <div className="w-full lg:w-2/3 px-5">
+                            <p className="pt-5">
+                                I already travelled to 37 countries and lived long-term in 3 of them — Brazil, Czech
+                                Republic and Germany — and had some short periods of my life in Greece and the US. I
+                                think
+                                that by expanding my own horizon and by discovering new realities, I can continuously be
+                                able to rediscover and reinvent myself. And yes, Japan and Iceland are my all time
+                                favorite
+                                countries!
+                            </p>
+                            <p className="pt-5">
+                                My family is my strength and motivation - I couldn&apos;t be half of the man I am today
+                                and have
+                                reached what I reached in my career and life if it wasn&apos;t for my amazing parents,
+                                grand-parents and my sisters. I dearly miss them (in portuguese - saudades) and
+                                constantly
+                                look forward to see them - which I am lucky to be able to do at least twice a year :)
+                            </p>
+                        </div>
+
+                        <div className="w-full lg:w-1/3 flex items-center justify-start px-5 md:pt-5 lg:pt-5 flex-col">
+                            <Image
+                                src="https://cdn.sanity.io/images/qgimqc2y/portfolio/cc750561393863c6a725eb6fc0b735f7a399eeed-871x1089.jpg"
+                                className="rounded-md text-center m-0 p-0 w-full h-full object-cover object-center shadow-2xl m-auto"
+                                alt="Gophercized Thiago's" title="Thiago's Gopher"
+                                height="300" width="300"
+                            />
+                            <figcaption className="text-xs mt-2">&quot;Joey doesn&apos;t share food&quot; - Berlin - 2021</figcaption>
+                        </div>
+                    </div>
+
+                    <h2 className="lg:hidden my-5 text-2xl text-center underline underline-offset-8 decoration-double decoration-green-500">Career
+                        & Technology</h2>
+                    <div className="flex flex-col-reverse md:flex-row md:mt-10">
+                        <div className="w-full lg:w-1/3 flex items-center justify-start px-5 md:pt-24 lg:pt-5 flex-col">
+                            <Image
+                                src="https://cdn.sanity.io/images/qgimqc2y/portfolio/4610af7ad05c653c0526926089a43ddcad473c24-943x959.jpg"
+                                className="rounded-md text-center m-0 p-0 w-full h-full object-cover object-center shadow-2xl"
+                                alt="Thiago's Photo taken in Kyoto - Japan Picture" title="Thiago in Kyoto - Japan"
+                                height="300" width="300"
+                            />
+                            <figcaption className="text-xs mt-2">Berlin - 2020</figcaption>
+                        </div>
+                        <div className='w-full lg:w-2/3 px-5 mb-10 md:mb-0'>
+                            <p className="pt-5">
+                                I consider Golang and Javascript (ReactJS/Vue.js) as my main programming languages — I
+                                love
+                                it! Prior to that I worked with PHP (Laravel), NodeJS and some other corporate languages
+                                (SAS Base, ACL...). I also really enjoy doing DevOps, Cloud Engineering with Terraform,
+                                AWS
+                                and Kubernetes. I see myself as a Solutions Engineer (<i>&quot;give me a problem and I
+                                will find
+                                a good solution&quot;</i>) and trail my career as a Full-stack developer - a position
+                                which
+                                enables me to traverse and learn the entire stack and have a full understanding of the
+                                solution with a greater impact.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col md:flex-row md:mt-5">
+                        <div className="px-5">
+                            <p className="pt-5">
+                                For the past months I&apos;ve taken a really big interest in Blockchain, Decentralized
+                                networks
+                                and dApps development, studying Rust and Solidity - the programming languages that power
+                                the
+                                main blockchains - I am fascinated with the possibilities of Blockchain and I believe
+                                that
+                                underneath the &quot;noise&quot; of cryptocurrencies it lies a technology that can
+                                definitely disrupt
+                                society in the same level the Internet did in the 90&apos;s/2000&apos;s.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col md:flex-row md:mt-5">
+                        <div className="w-full lg:w-2/3 px-5">
+                            <p className="pt-5">
+                                I tend to see and approach programming languages/technology as a means to an end —
+                                don&apos;t get
+                                me wrong, they are really important and need to be taken seriously — but let&apos;s not
+                                confuse
+                                the real purpose of it, which in my case, I believe it is to help, enable and unite
+                                people.
+                            </p>
+                            <p className="py-5">
+                                You can find more information about of my career on <Link href={linkedinLink}
+                                                                                          passHref><a
+                                target="_blank">LinkedIn</a></Link> or my CV
+                                (<Link href="/docs/Thiago_Mello_CV.pdf" passHref><a target="_blank">download
+                                here</a></Link>)
+                                and find some of my projects in <Link href={githubLink} passHref><a
+                                target="_blank">Github</a></Link>.
+                            </p>
+                        </div>
+                        <div className='w-full lg:w-1/3 flex items-end justify-center px-5'>
+                            <Image
+                                src="https://cdn.sanity.io/images/qgimqc2y/portfolio/f7b9f3e9404ce427b1a763d5d0ea0f3001e8f0bf-1300x1392.png?w=2000&fit=max&auto=format"
+                                className="text-center m-0 p-0 w-full h-full object-cover object-center"
+                                alt="Gophercized Thiago's" title="Thiago's Gopher"
+                                height="300" width="300"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
