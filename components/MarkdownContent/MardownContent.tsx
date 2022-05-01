7458dbf1a3ff175884ad1b8cfff16532d4935f58
Thiago <thiduzz14@gmail.com>
2022-05-02 00:18:55 +0200

    Attempt to fix hidration issue

--- components/MarkdownContent/MardownContent.tsx
+++ components/MarkdownContent/MardownContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactMarkdown from 'react-markdown'
 import {Light as SyntaxHighlighter} from 'react-syntax-highlighter'
 import {githubGist} from 'react-syntax-highlighter/dist/cjs/styles/hljs'
@@ -6,37 +6,51 @@ import remarkGfm from 'remark-gfm'
 import {MarkdownContentProps} from "@components/MarkdownContent/MarkdownContent.types";
 
 const MarkdownContent = ({children}: MarkdownContentProps) => {
-    return (<div className="markdown-content">
-        <ReactMarkdown
-            remarkPlugins={[[remarkGfm, {singleTilde: false}]]}
-            components={{
-                code({node, inline, className, children, ...props}) {
-                    const match = /language-(\w+)/.exec(className || '')
-                    return !inline && match ? (
-                        <SyntaxHighlighter
-                            style={githubGist}
-                            showLineNumbers={true}
-                            wrapLongLines={true}
-                            language={match[1]}
-                            PreTag="div"
-                            {...props}>
-                            {String(children).replace(/\n$/, '')}
-                        </SyntaxHighlighter>
-                    ) : (
-                        <code className={className} {...props}>
-                            {children}
-                        </code>
-                    )
-                },
-                img({node, src, title, alt }){
-                    if(src){
-                        return <img src={src} title={title} alt={alt} />
+    const [showChild, setShowChild] = useState(false);
+    useEffect(() => {
+        setShowChild(true);
+    }, []);
+
+    if (!showChild) {
+        return null;
+    }
+
+    if (typeof window === 'undefined') {
+        return <></>;
+    } else {
+
+        return (<div className="markdown-content">
+            <ReactMarkdown
+                remarkPlugins={[[remarkGfm, {singleTilde: false}]]}
+                components={{
+                    code({node, inline, className, children, ...props}) {
+                        const match = /language-(\w+)/.exec(className || '')
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                style={githubGist}
+                                showLineNumbers={true}
+                                wrapLongLines={true}
+                                language={match[1]}
+                                PreTag="div"
+                                {...props}>
+                                {String(children).replace(/\n$/, '')}
+                            </SyntaxHighlighter>
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        )
+                    },
+                    img({node, src, title, alt }){
+                        if(src){
+                            return <img src={src} title={title} alt={alt} />
+                        }
+                        return <div></div>
                     }
-                    return <div></div>
-                }
-            }}
-        >{children}</ReactMarkdown>
-    </div>)
+                }}
+            >{children}</ReactMarkdown>
+        </div>)
+    }
 }
 
 
